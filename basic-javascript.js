9482db3cebe1f47e25336d8261d04c6ca0a69851
kacm79 <105650651+kacm79@users.noreply.github.com>
2022-09-24 16:30:51 +0100

    Update basic-javascript.js

--- basic-javascript.js
+++ basic-javascript.js
@@ -32,3 +32,45 @@ if (strokes == 1) {
 }
 }
 console.log(golfScore(1, 1));
+
+// Counting cards
+
+et count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    count = count + 1;
+    break;
+    case 7:
+    case 8:
+    case 9:
+    count = count;
+    break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+    count = count - 1;
+    break;
+ }
+ if (count > 0){
+   return count + " Bet";
+ } else if (count < 1) {
+   return count + " Hold";
+ }
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+// Fibonnacci Generator from AppBrewery Web Dev Course
+
+
