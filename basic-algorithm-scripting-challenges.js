fe125b8aa54579fa2f9a25069c484328f6932de5
kacm79 <105650651+kacm79@users.noreply.github.com>
2022-10-24 09:26:43 +0100

    Update basic-algorithm-scripting-challenges.js

--- basic-algorithm-scripting-challenges.js
+++ basic-algorithm-scripting-challenges.js
@@ -6,3 +6,20 @@ function reverseString(str) {
   let newString = reverseArray.join("");
   return newString;
 }
+
+//Factorialise a number
+
+function factorialize(num) {
+
+const listOfIntegers = [];
+  for (let i = 1; i <= num; i++) {
+    listOfIntegers.push(i);
+  }
+
+let multipliedResult = 1;
+  for (let x = 0; x < num; x++) {
+    multipliedResult = multipliedResult*listOfIntegers[x];
+  }
+  return multipliedResult;
+}
+
